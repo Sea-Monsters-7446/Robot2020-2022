850a51d08d48cd7aa76113c243ee4dcf471f9d61
Harry N <94585779+big-harry@users.noreply.github.com>
2022-02-03 13:09:24 -0600

    Update Depends.sh

--- 2022/Depends.sh
+++ 2022/Depends.sh
@@ -5,6 +5,7 @@ sudo apt-get install git build-essential openjdk-11-jdk libc6-armel-cross binuti
 
 # This installs:
 # git - For interacting with the repo
+
 # build-essential - To build wpilibs
 # openjdk-11-jdk - Same as above
 
@@ -15,3 +16,10 @@ sudo apt-get install git build-essential openjdk-11-jdk libc6-armel-cross binuti
 
 # The compiler might also be found here:
 # frc/2022/roborio/bin/arm-frc2022-linux-gnueabi-gcc
+
+# This is for wpilibs, could just use the libraries directly but I don't feel like it
+wget https://github.com/wpilibsuite/roborio-toolchain/releases/download/v2022-1/FRC-2022-Linux-Toolchain-7.3.0.tar.gz
+tar -xf FRC*.tar.gz
+cd WPILib_Linux*
+./WPILibInstaller
+cd ..
