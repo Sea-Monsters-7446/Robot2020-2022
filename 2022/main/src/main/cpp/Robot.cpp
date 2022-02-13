34ab6fe05bee5eca6a508de4fb5eaa1e975ff6d1
Timothy Hutchins <98132840+TheTimBrick@users.noreply.github.com>
2022-02-12 21:41:04 -0600

    Just did stuff ya know (;
    Added a class that allows you to track button presses on the joystick and also added functions to control the intake, the conveyor, and the shooter.

--- 2022/main/src/main/cpp/Robot.cpp
+++ 2022/main/src/main/cpp/Robot.cpp
@@ -9,8 +9,101 @@
 #include <frc/Timer.h>
 #include <frc/XboxController.h>
 #include <frc/Joystick.h>
+#include <time.h>
+#include <frc2/command/button/JoystickButton.h>
+
+buttonState::buttonState(frc::Joystick& joystick) :
+  m_joystick(joystick)
+{
+
+}
+
+void buttonState::updateState() {
+  if (m_joystick.GetRawButtonPressed(1)) {
+    joystickButtonState.button1Pressed = true;
+  }
+  else if (m_joystick.GetRawButtonReleased(1)) {
+    joystickButtonState.button1Pressed = false;
+  }
+  if (m_joystick.GetRawButtonPressed(2)) {
+    joystickButtonState.button2Pressed = true;
+  }
+  else if (m_joystick.GetRawButtonReleased(2)) {
+    joystickButtonState.button2Pressed = false;
+  }
+  if (m_joystick.GetRawButtonPressed(3)) {
+    joystickButtonState.button3Pressed = true;
+  }
+  else if (m_joystick.GetRawButtonReleased(3)) {
+    joystickButtonState.button3Pressed = false;
+  }
+  if (m_joystick.GetRawButtonPressed(4)) {
+    joystickButtonState.button4Pressed = true;
+  }
+  else if (m_joystick.GetRawButtonReleased(4)) {
+    joystickButtonState.button4Pressed = false;
+  }
+  if (m_joystick.GetTopPressed()) {
+    joystickButtonState.topPressed = true;
+  }
+  else if (m_joystick.GetTopReleased()) {
+    joystickButtonState.topPressed = false;
+  }
+  if (m_joystick.GetTriggerPressed()) {
+    joystickButtonState.triggerPressed = true;
+  }
+  else if (m_joystick.GetTriggerReleased()) {
+    joystickButtonState.triggerPressed = false;
+  }
+
+}
+bool buttonState::isButton1Pressed() {
+
+  return joystickButtonState.button1Pressed;
+}
+
+bool buttonState::isButton2Pressed() {
+  return joystickButtonState.button2Pressed;
+}
+
+bool buttonState::isButton3Pressed() {
+  return joystickButtonState.button3Pressed;
+}
+
+bool buttonState::isButton4Pressed() {
+  return joystickButtonState.button4Pressed;
+}
+
+bool buttonState::isTopPressed() {
+  return joystickButtonState.topPressed;
+}
+
+bool buttonState::isTriggerPressed() {
+  return joystickButtonState.triggerPressed;
+}
+
+
+Robot::Robot() :
+  m_leftMotor1(1),
+  m_rightMotor1(2),
+  m_leftMotor2(3),
+  m_rightMotor2(4),
+  m_launcherMotor(5),
+  m_conveyor(6),
+  m_pickupMech1(7),
+  m_pickupMech2(8),
+  m_leftMotor(m_leftMotor1, m_leftMotor2),
+  m_rightMotor(m_rightMotor1, m_rightMotor2),
+  m_pickupMech(m_pickupMech1, m_pickupMech2),
+  m_drive(m_leftMotor, m_rightMotor),
+  m_joystick(0),
+  m_joystickButtons(m_joystick)
+{
+  
+}
 
 void Robot::RobotInit() {
+
 }
 
 /**
@@ -25,23 +118,13 @@ void Robot::RobotPeriodic() {
 
 }
 void Robot::AutonomousInit() {
-  // Resets the Timer
-  m_timer.Reset();
 }
 
 void Robot::AutonomousPeriodic() {
-  // Loops through and increases speed at an increment
-  for (double times = 0x00; times < 0x01; times = times + 0.000001) {
-  m_drive.ArcadeDrive(times, 0x00, true);
-  }
-  // Loops through and decreases speed at an increment
-  for (double times = 0x00; times > -1; times = times - 0.000001) {
-  m_drive.ArcadeDrive(times, 0x00, true);
-  }
+
 }
 
 void Robot::TeleopInit() {
-  m_timer.Reset();
 }
 
 void Robot::TeleopPeriodic() {
@@ -49,53 +132,54 @@ void Robot::TeleopPeriodic() {
   //m_drive.ArcadeDrive(m_controller.GetLeftY(), m_controller.GetRightX());
   // For use with joystick and simulation
 
-
   // Gets the X and Y of the joystick and controls the motor based on the values
   // The Y value of the joystick has to be reversed because the joystick is reversed
   // because of flight simulation and the way that is set up (up is down and down is up)
+  m_joystickButtons.updateState();
   m_drive.ArcadeDrive((m_joystick.GetY() * -1), m_joystick.GetX());
-
-  // Reports back to the SmartDashboard what direction the motor is supposed to be
-  // going in
-  if (m_joystick.GetX() > 0x00 && (m_joystick.GetY() * -1) > 0x00) {
-    frc::SmartDashboard::PutString("Direction:", "Right Forwards");
-  }
-  else if (m_joystick.GetX() > 0x00 &&(m_joystick.GetY() * -1) < 0x00) {
-    frc::SmartDashboard::PutString("Direction:", "Right Backwards");
-  }
-  else if (m_joystick.GetX() < 0x00 && (m_joystick.GetY() * -1) > 0x00) {
-    frc::SmartDashboard::PutString("Direction:", "Left Forwards");
-  }
-  else if (m_joystick.GetX() < 0x00 && (m_joystick.GetY() * -1) < 0x00) {
-    frc::SmartDashboard::PutString("Direction:", "Left Backwards");
-  }
-  else if (m_joystick.GetX() == 0x00 && (m_joystick.GetY() * -1) > 0x00) {
-    frc::SmartDashboard::PutString("Direction:", "Straight Forwards");
-  }
-  else if (m_joystick.GetX() == 0x00 && (m_joystick.GetY() * -1) < 0x00) {
-    frc::SmartDashboard::PutString("Direction:", "Straight Backwards");
-  }
-  else if (m_joystick.GetX() > 0x00 && (m_joystick.GetY() * -1) == 0x00) {
-    frc::SmartDashboard::PutString("Direction:", "Right");
-  }
-  else if (m_joystick.GetX() < 0x00 && (m_joystick.GetY() * -1) == 0x00) {
-    frc::SmartDashboard::PutString("Direction:", "Left");
+  
+  if (m_joystickButtons.isButton1Pressed()) {
+    if (m_joystickButtons.isButton4Pressed()) {
+      m_pickupMech.Set(-0.5);
+    }
+    else {
+      m_pickupMech.Set(0.5);
+    }
   }
   else {
-    frc::SmartDashboard::PutString("Direction:", "Stationary");
+    m_pickupMech.Set(0);
   }
 
-}
+  if (m_joystickButtons.isButton2Pressed()) {
+    if (m_joystickButtons.isButton4Pressed()) {
+      m_conveyor.Set(-0.5);
+    }
+    else {
+      m_conveyor.Set(0.5);
+    }
+  }
+  else {
+    m_conveyor.Set(0);
+  }
 
+  if (m_joystickButtons.isButton3Pressed()) {
+    if (m_joystickButtons.isButton4Pressed()) {
+      m_launcherMotor.Set(-1);
+    }
+    else {
+      m_launcherMotor.Set(1);
+    }
+  }
+  else {
+    m_launcherMotor.Set(0);
+  }
+}
 void Robot::DisabledInit() {
-  // Starts a timer
-  m_timer.Start();
+
 }
 
 void Robot::DisabledPeriodic() {
-  // Prints back the number of seconds we have been in disabled mode to 
-  // SmartDashboard
-  frc::SmartDashboard::PutNumber("Time Disabled:", m_timer.Get().value());
+
 }
 
 void Robot::TestInit() {
--- 2022/main/src/main/include/Robot.h
+++ 2022/main/src/main/include/Robot.h
@@ -13,9 +13,38 @@
 #include <frc/Joystick.h>
 #include <frc/motorcontrol/PWMVictorSPX.h>
 #include <frc/motorcontrol/MotorControllerGroup.h>
+#include <frc2/command/button/JoystickButton.h>
+
+class buttonState {
+  public:
+    buttonState(frc::Joystick& joystick);
+    void updateState();
+    bool isButton1Pressed();
+    bool isButton2Pressed();
+    bool isButton3Pressed();
+    bool isButton4Pressed();
+    bool isTriggerPressed();
+    bool isTopPressed();
+
+  private:
+    struct m_buttonState {
+      bool topPressed;
+      bool triggerPressed;
+      bool button1Pressed;
+      bool button2Pressed;
+      bool button3Pressed;
+      bool button4Pressed;
+
+    };
+    m_buttonState joystickButtonState;
+    frc::Joystick& m_joystick;
+
+};
 
 class Robot : public frc::TimedRobot {
  public:
+  Robot();
+
   void RobotInit() override;
   void RobotPeriodic() override;
   void AutonomousInit() override;
@@ -28,14 +57,26 @@ class Robot : public frc::TimedRobot {
   void TestPeriodic() override;
 
  private:
-  frc::PWMVictorSPX m_leftMotor1{1};
-  frc::PWMVictorSPX m_rightMotor1{2};
-  frc::PWMVictorSPX m_leftMotor2{3};
-  frc::PWMVictorSPX m_rightMotor2{4};
-  frc::MotorControllerGroup m_leftMotor{m_leftMotor1, m_leftMotor2};
-  frc::MotorControllerGroup m_rightMotor{m_rightMotor1, m_rightMotor2};
+  // Motors for the drive wheels
+  frc::PWMVictorSPX m_leftMotor1;
+  frc::PWMVictorSPX m_rightMotor1;
+  frc::PWMVictorSPX m_leftMotor2;
+  frc::PWMVictorSPX m_rightMotor2;
+  // Motor for the launcher
+  frc::PWMVictorSPX m_launcherMotor;
+  // Motor for the conveyor belt
+  frc::PWMVictorSPX m_conveyor;
+  // Motors for the pickup mechanism
+  frc::PWMVictorSPX m_pickupMech1;
+  frc::PWMVictorSPX m_pickupMech2;
 
-  frc::DifferentialDrive m_drive{m_leftMotor, m_rightMotor};
-  frc::Timer m_timer;
-  frc::Joystick m_joystick{0};
+  frc::MotorControllerGroup m_leftMotor;
+  frc::MotorControllerGroup m_rightMotor;
+  // Motor group for the ball pickup mechanism
+  frc::MotorControllerGroup m_pickupMech;
+  
+  frc::DifferentialDrive m_drive;
+  frc::Joystick m_joystick;
+
+  buttonState m_joystickButtons;
 };
