b835a7d0413c578131316ad4c2ce842fcc0b8510
Timothy Hutchins <98132840+TheTimBrick@users.noreply.github.com>
2022-02-14 10:32:24 -0600

    Documented code more and made buttonState class more efficient and code-clean

--- 2022/main/src/main/cpp/buttonState.cpp
+++ 2022/main/src/main/cpp/buttonState.cpp
@@ -1,12 +1,19 @@
 #include "buttonState.h"
 #include <frc/Joystick.h>
-
+/**
+ * @brief Construct a new buttonState object
+ * 
+ * @param joystick A joystick object created from frc/Joystick.h
+ */
 buttonState::buttonState(frc::Joystick& joystick) :
   m_joystick(joystick)
 {
 
 }
-
+/**
+ * @brief Updates the button states
+ * 
+ */
 void buttonState::updateState() {
   if (m_joystick.GetRawButtonPressed(1)) {
     joystickButtonState.button1Pressed = true;
@@ -46,27 +53,63 @@ void buttonState::updateState() {
   }
 
 }
+/**
+ * @brief Checks if button 1 of the joystick is pressed
+ * 
+ * @return true 
+ * @return false 
+ */
 bool buttonState::isButton1Pressed() {
-
+  updateState();
   return joystickButtonState.button1Pressed;
 }
-
+/**
+ * @brief Checks if button 2 of the joystick is pressed
+ * 
+ * @return true 
+ * @return false 
+ */
 bool buttonState::isButton2Pressed() {
+  updateState();
   return joystickButtonState.button2Pressed;
 }
-
+/**
+ * @brief Checks if button 3 of the joystick is pressed
+ * 
+ * @return true 
+ * @return false 
+ */
 bool buttonState::isButton3Pressed() {
+  updateState();
   return joystickButtonState.button3Pressed;
 }
-
+/**
+ * @brief Checks if button 4 of the joystick is pressed
+ * 
+ * @return true 
+ * @return false 
+ */
 bool buttonState::isButton4Pressed() {
+  updateState();
   return joystickButtonState.button4Pressed;
 }
-
+/**
+ * @brief Checks to see if the top button of the joystick is pressed
+ * 
+ * @return true 
+ * @return false 
+ */
 bool buttonState::isTopPressed() {
+  updateState();
   return joystickButtonState.topPressed;
 }
-
+/**
+ * @brief Checks to see if the trigger of the joystick is pressed
+ * 
+ * @return true 
+ * @return false 
+ */
 bool buttonState::isTriggerPressed() {
+  updateState();
   return joystickButtonState.triggerPressed;
 }
