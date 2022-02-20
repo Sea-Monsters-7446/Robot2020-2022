cf1278a7fe17f09af7f872f48df70a9348c63b2e
Timothy Hutchins <98132840+TheTimBrick@users.noreply.github.com>
2022-02-19 18:58:24 -0600

    Created template for circle detection

--- 2022/main/src/main/cpp/Vision.cpp
+++ 2022/main/src/main/cpp/Vision.cpp
@@ -27,6 +27,21 @@ void VisionSense::operator()() {
             output.NotifyError(sync.GetError());
             continue;
         }
+
+        // Say the X and Y are the coordinate of the center of the closest ball from [-1, -1] to [1, 1]
+        double closestCircleX = 0.0;
+        double closestCircleY = 0.0;
+        double closestCircleDistance = 0.0;
+
+        // Use openCV to identify circles (hough algorithm)
+        // see https://docs.opencv.org/3.4/d4/d70/tutorial_hough_circle.html
+
+        // Then calculate the distance of the circle based on the known radius of the ball
+        // Calculate the X, and Y of where the center of the ball in the viewport.
+
+        // Set the ball information
+        m_data.set(std::make_tuple(closestCircleX, closestCircleY, closestCircleDistance));
+        
         output.PutFrame(matrix);
     }
 }
