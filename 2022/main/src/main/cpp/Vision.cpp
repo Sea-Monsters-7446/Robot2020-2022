129ba90a8025af04dde12feac0cf2e752fcae64b
Timothy Hutchins <98132840+TheTimBrick@users.noreply.github.com>
2022-02-22 21:24:27 -0600

    Hopefully got circle detection which will be used to find balls during autonomous mode. Also Fixed Doxygen.

--- 2022/main/src/main/cpp/Vision.cpp
+++ 2022/main/src/main/cpp/Vision.cpp
@@ -1,11 +1,15 @@
 #include "Vision.h"
-
 #include <cameraserver/CameraServer.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/core/types.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
+#include <frc/smartdashboard/SmartDashboard.h>
 #include <atomic>
 #include <utility>
+#include <vector>
+#include <cstdio>
 #include "SafeData.hpp"
 /**
  * @brief Construct a new `VisionSense` object
@@ -26,29 +30,28 @@ void VisionSense::operator()() {
     cs::UsbCamera camera = frc::CameraServer::StartAutomaticCapture(0);
     camera.SetResolution(640, 480);
     cs::CvSink sync = frc::CameraServer::GetVideo();
-    cs::CvSource output = frc::CameraServer::PutVideo("VisionSense", 640, 480);
-    cv::Mat matrix;
+    cs::CvSource output = frc::CameraServer::PutVideo("VisionSense Detected Circles", 640, 480);
+    cv::Mat fullMatrix;
+    std::vector<cv::Mat> rgbChannels;
     m_isRunning = true;
     while (m_isRunning) {
-        if (sync.GrabFrame(matrix) == 0) {
+        if (sync.GrabFrame(fullMatrix) == 0) {
             output.NotifyError(sync.GetError());
             continue;
         }
-
-        // Say the X and Y are the coordinate of the center of the closest ball from [-1, -1] to [1, 1]
-        double closestCircleX = 0.0;
-        double closestCircleY = 0.0;
-        double closestCircleDistance = 0.0;
-
-        // Use openCV to identify circles (hough algorithm)
-        // see https://docs.opencv.org/3.4/d4/d70/tutorial_hough_circle.html
-
-        // Then calculate the distance of the circle based on the known radius of the ball
-        // Calculate the X, and Y of where the center of the ball in the viewport.
-
-        // Set the ball information
-        m_data.set(std::make_tuple(closestCircleX, closestCircleY, closestCircleDistance));
-        output.PutFrame(matrix);
+        std::vector<cv::Vec3f> circles;
+        cv::split(fullMatrix, rgbChannels);
+        cv::HoughCircles(rgbChannels.at(1), circles, cv::HoughModes::HOUGH_STANDARD, 1, ((rgbChannels.at(1).rows) / 4), 200, 100);
+        for(unsigned int times = 0; times < circles.size(); times++) {
+            cv::Vec3i c = circles[times];
+            cv::Point center = cv::Point(c[0], c[1]);
+            // circle center
+            cv::circle(fullMatrix, center, 1, cv::Scalar(0,100,100), 3, cv::LINE_AA);
+            // circle outline
+            int radius = c[2];
+            cv::circle(fullMatrix, center, radius, cv::Scalar(255,0,255), 3, cv::LINE_AA);
+        }
+        output.PutFrame(fullMatrix);
     }
 }
 /**
