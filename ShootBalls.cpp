636138bd5c718e1499a36e79428eb3480e14cc3e
Atherolite <athliteyg@gmail.com>
2022-03-03 17:06:57 -0600

    Add files via upload
    code for autonomously shooting balls, WIP

---
+++ ShootBalls.cpp
@@ -0,0 +1,16 @@
+#include "YeeterControl.h"
+#include "Autonomous.h"
+
+class shootBalls : public frc::YeeterControl {
+    public:
+    
+}
+
+int Autonomous::start() {
+    if() {
+
+    }
+    else {
+        cout << "No.";
+    }
+}
